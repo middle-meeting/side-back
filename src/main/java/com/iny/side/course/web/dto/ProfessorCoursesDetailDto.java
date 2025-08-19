bdbb3f2f6359c13a6945af2767863a5fff4810b9
choiapple <79374648+choiapple@users.noreply.github.com>
2025-08-20 00:11:57 +0900

    feat : course에 column 추가, 교수 강의 상세 dto에 추가

--- src/main/java/com/iny/side/course/web/dto/ProfessorCoursesDetailDto.java
+++ src/main/java/com/iny/side/course/web/dto/ProfessorCoursesDetailDto.java
@@ -2,14 +2,18 @@ package com.iny.side.course.web.dto;
 
 import com.iny.side.course.domain.entity.Course;
 
-public record ProfessorCoursesDetailDto(Long id, String name, String semester, String description, String professorName) {
+public record ProfessorCoursesDetailDto(Long id, String name, String semester, String description, String professorName, Integer totalStudents, String schedule, String classroom, String department) {
     public static ProfessorCoursesDetailDto from(Course course){
         return new ProfessorCoursesDetailDto(
                 course.getId(),
                 course.getName(),
                 course.getSemester(),
                 course.getDescription(),
-                course.getAccount().getName()
+                course.getAccount().getName(),
+                course.getTotalStudents(),
+                course.getSchedule(),
+                course.getClassroom(),
+                course.getDepartment()
         );
     }
 }
